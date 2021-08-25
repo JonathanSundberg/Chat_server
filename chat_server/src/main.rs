b9d23da7c6fd41d95853a40857bdbcacaffea1ad
Jonathan Sundberg <guitaraddict95@hotmail.com>
2021-08-25 23:18:35 +0200

    added unit tests and basic test case

--- chat_server/src/main.rs
+++ chat_server/src/main.rs
@@ -1,4 +1,5 @@
 #![feature(proc_macro_hygiene, decl_macro)]
+use rocket::http::RawStr;
 
 #[macro_use] extern crate rocket;
 
@@ -7,6 +8,45 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/message/<sent_string>")]
+fn get_messages(sent_string: &RawStr) -> String{
+    format!("This is the string:, {}", sent_string.as_str())
+}
+
+#[get("/message/<sent_string>")]
+fn send_message(sent_string: &RawStr) -> String{
+    format!("This is the string:, {}", sent_string.as_str())
+}
+
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite()
+        .mount("/", routes![index])
+        .mount("/", routes![get_messages])
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket().launch();
+}
+
+// use cargo test -- --nocapture to get output
+#[cfg(test)]
+mod tests{
+    use super::rocket;
+    use rocket::local::Client;
+    use rocket::http::Status;
+
+    #[test]
+    fn get_messages(){
+        let client = Client::new(rocket()).expect("valid rocket instance");
+        let mut response = client.get("/").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        //assert_eq!(response.body_string(), Some("Hello, world!".into()));
+
+        println!("message string: {}", response.body_string().unwrap());
+    }
+
+    fn send_message(){
+
+    }
 }
