4b41d50a4d89280e97a7a23d8170ace8c1508c6d
Jonathan Sundberg <guitaraddict95@hotmail.com>
2021-08-27 10:41:37 +0200

    Adding more dependencies and creating base for post request handle

--- chat_server/src/main.rs
+++ chat_server/src/main.rs
@@ -1,28 +1,41 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 use rocket::http::RawStr;
-
+use serde::Deserialize;
+use serde_json;
+use rocket_contrib::json::Json;
 #[macro_use] extern crate rocket;
+
+
+#[derive(FromForm, Deserialize)]
+struct Message{
+    message: String,
+    user: String,
+
+}
+
+
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/message/<sent_string>")]
-fn get_messages(sent_string: &RawStr) -> String{
+#[get("/message/update/<sent_string>")]
+fn update_messages(sent_string: &RawStr) -> String{
     format!("This is the string:, {}", sent_string.as_str())
 }
 
-#[get("/message/<sent_string>")]
-fn send_message(sent_string: &RawStr) -> String{
-    format!("This is the string:, {}", sent_string.as_str())
+#[post("/message/received", format = "json")]
+fn received_message() -> String{
+    String::from("asd")
 }
 
 
 fn rocket() -> rocket::Rocket {
     rocket::ignite()
         .mount("/", routes![index])
-        .mount("/", routes![get_messages])
+        .mount("/", routes![update_messages])
+        .mount("/", routes![received_message])
 }
 
 fn main() {
@@ -37,7 +50,7 @@ mod tests{
     use rocket::http::Status;
 
     #[test]
-    fn get_messages(){
+    fn update_messages(){
         let client = Client::new(rocket()).expect("valid rocket instance");
         let mut response = client.get("/").dispatch();
         assert_eq!(response.status(), Status::Ok);
@@ -46,7 +59,7 @@ mod tests{
         println!("message string: {}", response.body_string().unwrap());
     }
 
-    fn send_message(){
+    fn received_message(){
 
     }
 }
